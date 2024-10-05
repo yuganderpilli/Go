f304de8b85c84c1e97d5796495b84a9063a22572
yuganderpilli <yuganderpilli@gmail.com>
2024-10-06 00:12:42 +0530

    notes update

--- Three/main.go
+++ Three/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 	"unicode/utf8"
 )
 
@@ -15,4 +17,15 @@ func main() {
 	for i, runeVal := range rStr {
 		fmt.Printf("%d:%#U:%c \n", i, runeVal, runeVal)
 	}
+	now := time.Now()
+	pl(now)
+	pl(now.Year())
+	pl(now.Month())
+	pl(now.Day())
+	seedSecs := now.Unix() // gives second until 1 jan 1970
+	pl(seedSecs)
+	// generating random number using the seed seconds value as the seed
+	randNum := rand.Intn(51)
+	pl(randNum)
+
 }
